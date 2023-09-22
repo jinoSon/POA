8e1e879bc444e2f6c38ac4a84156127140979dea
Jino Son <jino.son@samsung.com>
2023-09-23 01:41:30 +0900

    fixed dallor

--- exchange/bitget.py
+++ exchange/bitget.py
@@ -97,7 +97,8 @@ class Bitget:
             result = order_info.amount
         elif order_info.dallor is not None:
             current_price = self.get_price(order_info.unified_symbol)
-            result =  order_info.dallor *  order_info.leverage / current_price
+            order_info.amount = order_info.dallor *  order_info.leverage / current_price
+            result = order_info.amount
         elif order_info.percent is not None:
             if order_info.is_entry or (order_info.is_spot and order_info.is_buy):
                 free_quote = self.get_balance(order_info.quote)
